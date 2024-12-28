000703767ebba5d206308263cf14a18056f4d56a
nathfavour <116535483+nathfavour@users.noreply.github.com>
2024-12-28 21:29:14 +0100

    🔨 fix: update description in layout component for clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Deeper sensor",
-  description: "",
+  description: "Useful opensource AI robots, better world for everyone.",
 };
 
 export default function RootLayout({
